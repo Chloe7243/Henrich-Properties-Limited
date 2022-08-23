ab5c1d2580fbde362bd5593b5212fa9ac211391c
delgo123 <75376053+delgo123@users.noreply.github.com>
2022-08-23 08:36:14 +0100

    update

--- Scripts/script.js
+++ Scripts/script.js
@@ -106,3 +106,18 @@ buttons.forEach((button) => {
         localStorage.setItem("id", button.dataset.id);
     });
 });
+
+//IMAGE SLIDER
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
